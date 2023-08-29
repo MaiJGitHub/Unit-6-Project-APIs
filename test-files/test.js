f0be10d7cc051a5a566e6681fbb05a6b65313aab
MaiJGitHub <jmai112302@gmail.com>
2023-08-29 10:08:20 -0400

    update

--- test-files/test.js
+++ test-files/test.js
@@ -1,15 +1,3 @@
-const gamesUrl = 'https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cb7c281af5mshed3ac801f114632p183454jsn726a4e5c6429',
-		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-	}
-};
-
-const musicUrl = 'https://player.fm/featured/true-crime.json?episode_detail=full'
-
-
 const showApi = async () => {
     try {
         const gameRes = await fetch(gamesUrl, options);
